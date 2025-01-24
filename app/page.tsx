7b039e0152d4a73a1c335aa53acc603fe8d9d487
yama-0011 <162927467+yama-0011@users.noreply.github.com>
2025-01-24 07:38:18 +0000

    env.に変更

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default function Page() {
             <span>/dashboard</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
           <Link
-            href="/query/seed"
+            href="/seed"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>DB</span> <ArrowRightIcon className="w-5 md:w-6" />
