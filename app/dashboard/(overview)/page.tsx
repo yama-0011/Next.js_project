2a0b05cc3a36912158bdbce86274356877a76a7e
yama-0011 <162927467+yama-0011@users.noreply.github.com>
2025-01-30 08:44:17 +0000

    ストリーミング対応2

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,11 +2,14 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+import { fetchCardData } from '@/app/lib/data';// remove fetchRevenue,fetchlatestInvoices
+import { Suspense } from 'react';
+import { RevenueChartSkeleton , LatestInvoicesSkeleton , CardSkeleton } from '@/app/ui/skeletons';
+import CardWrapper from '@/app/ui/dashboard/cards';
  
 export default async function Page() {
-  const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
+  // const revenue = await fetchRevenue();//ストリーミング対応で削除
+  // const latestInvoices = await fetchLatestInvoices();//ストリーミング対応で削除
   const {
     numberOfCustomers,
     numberOfInvoices,
@@ -29,8 +32,18 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue}  />
-        <LatestInvoices latestInvoices={latestInvoices} />
+      <Suspense fallback={<RevenueChartSkeleton />}>
+        <RevenueChart />
+      </Suspense>
+
+      <Suspense fallback={<LatestInvoicesSkeleton />}>
+        <LatestInvoices />
+      </Suspense>
+
+      <Suspense fallback={<CardSkeleton />}>
+        <CardWrapper />
+      </Suspense>
+
       </div>
     </main>
   );
--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -3,11 +3,15 @@ import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
 import { LatestInvoice } from '@/app/lib/definitions';
-export default async function LatestInvoices({
-  latestInvoices,
-}: {
-  latestInvoices: LatestInvoice[];
-}) {
+import { fetchLatestInvoices } from '@/app/lib/data';
+
+export default async function LatestInvoices(){// コンポーネントを非同期にし、props を削除する。
+//   latestInvoices,
+// }: {
+//   latestInvoices: LatestInvoice[];
+// }) {
+const latestInvoices = await fetchLatestInvoices(); // コンポーネント内部のデータを取得する
+
   return (
     <div className="flex w-full flex-col md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
