5b8a42c74f8385bbc72420bce014ce6abc34a694
yama-0011 <162927467+yama-0011@users.noreply.github.com>
2025-02-19 08:49:07 +0000

    サーバーアクション設定の微修正

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,4 +1,5 @@
 'use server';
+
 import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
@@ -16,21 +17,17 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
-        customerId: formData.get('customerId'),
-        amount: formData.get('amount'),
-        status: formData.get('status'),
-    });
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+  await sql`
+  INSERT INTO invoices (customer_id, amount, status, date)
+  VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
 
-      // Test it out:
-    //   console.log(rawFormData);
-
-    const amountInCents = amount * 100;
-    const date = new Date().toISOString().split('T')[0];
-     await sql`
-      INSERT INTO invoices (customer_id, amount, status, date)
-      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
-
-      revalidatePath('/dashboard/invoices');
-      redirect('/dashboard/invoices');
+revalidatePath('/dashboard/invoices');
+redirect('/dashboard/invoices');
 }
