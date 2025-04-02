d1b04fc52ce390f33be0b2aa1a2aa2102742d771
yama-0011 <162927467+yama-0011@users.noreply.github.com>
2025-04-02 06:51:51 +0000

    Acmeメイン画面のフォントサイズ修正

--- app/page.tsx
+++ app/page.tsx
@@ -17,11 +17,11 @@ export default function Page() {
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
         <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"/>
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+          <strong className="text-[20px]">Acme.へようこそ。</strong> <strong className="text-[20px]">これはVercelが提供する{' '}</strong>
+            <a href="https://nextjs.org/learn/" className="text-blue-500 text-[20px]">
+            Next.js学習コース
             </a>
-            , brought to you by Vercel.
+            <strong className="text-[20px]">の例です。</strong>
           </p>
           <Link
             href="/login"
